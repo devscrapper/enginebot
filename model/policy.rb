d8efc9b17bc5351f4f6d00260f9e97ea6f5862e3
egelin <devscrapper@yahoo.fr>
2012-12-11 13:22:23 +0100

    v0.4 (suite)

---
+++ model/policy.rb
@@ -0,0 +1,56 @@
+require 'net/http'
+require 'uri'
+require File.dirname(__FILE__) + '/../lib/logging'
+require File.dirname(__FILE__) + '/../lib/common'
+class Policy
+
+  attr :label,
+       :date
+
+  def initialize(label, date)
+    #TODO securiser le fait qu'il ne peut y avoir 2 profils pour un website pour un jour donné lors de la creation du profil avec un callback
+     @label = label
+    @date = date
+  end
+
+
+  def properties()
+    result = select
+    [result["change_count_visits_percent"],
+     result["change_bounce_visits_percent"],
+    result["direct_medium_percent"],
+    result["organic_medium_percent"],
+    result["referral_medium_percent"],
+    ]
+  end
+
+
+  #private
+  def select()
+    begin
+      #http://localhost:3000/websites/epilation-laser-definitive/policies/2012-11-26/select.json
+      url = "http://#{$statupweb_server_ip}:#{$statupweb_server_port}/websites/#{@label}/policies/#{@date}/select.json"
+      resp = Net::HTTP.get_response(URI.parse(url))
+      if  resp.is_a?(Net::HTTPSuccess) and !(resp.body == "null")
+          res = JSON.parse(resp.body)
+          Common.information("getting policy websites = #{@label}, objectives = #{@date} from statupweb #{$statupweb_server_ip}:#{$statupweb_server_port} is success")
+      else
+      if resp.is_a?(Net::HTTPSuccess) and resp.body == "null"
+          res = {}
+          Common.alert("getting policy websites = #{@label}, objectives = #{@date} from statupweb #{$statupweb_server_ip}:#{$statupweb_server_port} not found")
+      else
+      if !resp.is_a?(Net::HTTPSuccess)
+          res = {}
+          Common.alert("getting policy websites = #{@label}, objectives = #{@date} from statupweb #{$statupweb_server_ip}:#{$statupweb_server_port} failed : http error : #{resp.error}")
+      end
+      end
+      end
+      res
+    rescue Exception => e
+      Common.alert("getting policy from statupweb #{$statupweb_server_ip}:#{$statupweb_server_port} failed : #{e.message}", __LINE__)
+      {}
+    end
+  end
+
+
+end
