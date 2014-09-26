e36ba3a40e79d5345b12eef277fbf0da93bbfaa5
dave <devscrapper@yahoo.fr>
2014-09-26 09:14:15 +0200

    v0.40 mise en place geolocation : -pas de calcul pour le moment -diffusion du fichier toutes les heures vers tous les statupbot
    remplacement des constants STDOUT, STDERR par les variables globales : $stdout $stderr
    
    suppression de la log dans reporting et mail_sender

--- model/geolocation.rb
+++ model/geolocation.rb
@@ -3,14 +3,21 @@ class Geolocation
   TMP = Pathname(File.join(File.dirname(__FILE__), '..', 'tmp')).realpath
 
 
+  # les proxy sont stockés dans un fichier geolocations_#{staging} par staging : alimentation pour le moment manuelle
+  # les proxy sont publiés dans OUTPUT toutes les heures avec autant d'occurence de fichier que serveur input_flow qui réceptionne le fichier
+  # autrement dit il est autant d'occurence de geolocations_#{staging}_today_hour.txt que d'instance d'os/version en ligne décrite dans le
+  # fichier de paramètage du serveur scheduler_server.
   def self.send(inputflow_factories, authentification_server_port, ftp_server_port, logger)
     inputflow_factories.each { |os_label, version|
       version.each { |version_label, input_flow_servers|
         input_flow_servers[:servers].each_value { |input_flow_server|
           begin
+            geo_file_source = "geolocations_#{$staging}.txt"
             geolocations_flow = Flow.from_basename(OUTPUT, "geolocations_#{$staging}_#{Date.today.strftime("%Y-%m-%d")}_#{Time.now.hour}.txt")
 
-            FileUtils.cp(File.join(TMP, "geolocations_#{$staging}.txt"), geolocations_flow.absolute_path)
+            raise "flow <#{geo_file_source}> not exist" unless File.exist?(geo_file_source)
+
+            FileUtils.cp(File.join(TMP, geo_file_source), geolocations_flow.absolute_path)
 
             geolocations_flow.push(authentification_server_port,
                                    input_flow_server[:ip],
@@ -20,9 +27,9 @@ class Geolocation
                                    true)
 
           rescue Exception => e
-            logger.a_log.error "geolocations flow not push to input flow server #{input_flow_server[:ip]}:#{input_flow_server[:port]} : #{e.message}"
+            logger.a_log.error "geolocations flow <#{geolocations_flow.basename}> not push to input flow server #{input_flow_server[:ip]}:#{input_flow_server[:port]} : #{e.message}"
           else
-            logger.a_log.info "geolocations flow push to input flow server #{input_flow_server[:ip]}:#{input_flow_server[:port]}"
+            logger.a_log.info "geolocations flow <#{geolocations_flow.basename}> push to input flow server #{input_flow_server[:ip]}:#{input_flow_server[:port]}"
           end
         }
       }
