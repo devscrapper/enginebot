eb557df47f8faf52b9ebefac144c31d9b583fb2c
dave <devscrapper@yahoo.fr>
2015-11-22 17:05:48 +0100

    v0.47
    creer les fichiers landing page vide au cas oùles traffics sources soient vides

--- model/tasking/event2task/inputs.rb
+++ model/tasking/event2task/inputs.rb
@@ -62,20 +62,24 @@ module Tasking
 
       landing_page_type_flow = "landing-pages-#{medium.to_s}"
       traffic_source_file = Flow.last(INPUT, {:type_flow => traffic_source_type_flow,
-                                           :label => @label,
-                                           :policy => @policy_type}).last #input
+                                              :label => @label,
+                                              :policy => @policy_type}).last #input
       @logger.an_event.debug "traffic source type flow : #{traffic_source_file.basename}"
       raise IOError, "input flow <#{traffic_source_file.basename}> is missing" unless traffic_source_file.exist? #input
 
       landing_pages_file = Flow.new(TMP, landing_page_type_flow, @policy_type, @label, @date_building) #output
+      # on creer le fichier vide.
+      # cela permet d'avoir un landing initialisé même si le traffic_source est vide.
+      landing_pages_file.empty
+
 
       total = 0
-      traffic_source_file.volumes.each { |volume| total += volume.count_lines(EOFLINE)}
+      traffic_source_file.volumes.each { |volume| total += volume.count_lines(EOFLINE) }
 
       pob = ProgressBar.create(:title => title("Building landing #{medium.to_s}"), :length => PROGRESS_BAR_SIZE, :starting_at => 0, :total => total, :format => '%t, %c/%C, %a|%w|')
       traffic_source_file.volumes.each { |volume|
         # @logger.an_event.info "Loading vol <#{volume.vol}> of #{traffic_source_file.basename} input file"
-#        pob = ProgressBar.create(:title => "Loading vol <#{volume.vol}> of #{traffic_source_file.basename} input file", :length => PROGRESS_BAR_SIZE, :starting_at => 0, :total => volume.count_lines(EOFLINE), :format => '%t, %c/%C, %a|%w|')
+        #        pob = ProgressBar.create(:title => "Loading vol <#{volume.vol}> of #{traffic_source_file.basename} input file", :length => PROGRESS_BAR_SIZE, :starting_at => 0, :total => volume.count_lines(EOFLINE), :format => '%t, %c/%C, %a|%w|')
         volume.foreach(EOFLINE) { |p|
           source_page = yield(p)
           landing_pages_file.write(source_page.to_s)
@@ -86,6 +90,7 @@ module Tasking
       landing_pages_file.close
       landing_pages_file.archive_previous
     end
+
     private
     def title(action, policy = @policy_type, label = @label, date = @date_building)
       [action, policy, label, date].join(" | ")
