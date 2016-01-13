ac2998daf0b0cc1343fb45b0b60d2a5890b6fd29
dave <devscrapper@yahoo.fr>
2016-01-13 12:06:25 +0100

    v0.62
    utilisation de la lib parameter pour calendar_server

--- run/calendar_server.rb
+++ run/calendar_server.rb
@@ -1,48 +1,39 @@
-#!/usr/bin/env ruby -w
 # encoding: UTF-8
 require 'yaml'
 require 'rufus-scheduler'
 require_relative '../lib/logging'
+require_relative '../lib/parameter'
 require_relative '../model/planning/calendar'
 require_relative '../model/planning/connection'
 
 
 #--------------------------------------------------------------------------------------------------------------------
-# INIT
-#--------------------------------------------------------------------------------------------------------------------
-PARAMETERS = File.dirname(__FILE__) + "/../parameter/" + File.basename(__FILE__, ".rb") + ".yml"
-ENVIRONMENT= File.dirname(__FILE__) + "/../parameter/environment.yml"
-listening_port = 9104
-scrape_server_port = 9101
-periodicity = "0 0 * * * 1-7 Europe/Paris"
-$staging = "production"
-$debugging = false
-#--------------------------------------------------------------------------------------------------------------------
-# INPUT
+# LOAD PARAMETER
 #--------------------------------------------------------------------------------------------------------------------
 begin
-  environment = YAML::load(File.open(ENVIRONMENT), "r:UTF-8")
-  $staging = environment["staging"] unless environment["staging"].nil?
-rescue Exception => e
-  $stderr << "loading parameter file #{ENVIRONMENT} failed : #{e.message}"  << "\n"
-end
-  #TODO utiliser la librairie parameter
-begin
-  params = YAML::load(File.open(PARAMETERS), "r:UTF-8")
-  listening_port = params[$staging]["listening_port"] unless params[$staging]["listening_port"].nil?
-  scrape_server_port = params[$staging]["scrape_server_port"] unless params[$staging]["scrape_server_port"].nil?
-  periodicity = params[$staging]["periodicity"] unless params[$staging]["periodicity"].nil?
-  $debugging = params[$staging]["debugging"] unless params[$staging]["debugging"].nil?
-rescue Exception => e
-  $stderr << "loading parameters file #{PARAMETERS} failed : #{e.message}" << "\n"
-end
+  parameters = Parameter.new(__FILE__)
 
+rescue Exception => e
+  $stderr << e.message << "\n"
+
+else
+  $staging = parameters.environment
+  $debugging = parameters.debugging
+  listening_port = parameters.listening_port
+  periodicity = parameters.periodicity
+
+  if listening_port.nil? or
+      periodicity.nil? or
+      $debugging.nil? or
+      $staging.nil?
+    $stderr << "some parameters not define" << "\n"
+    exit(1)
+  end
+end
 logger = Logging::Log.new(self, :staging => $staging, :id_file => File.basename(__FILE__, ".rb"), :debugging => $debugging)
-
 
 logger.a_log.info "parameters of calendar server :"
 logger.a_log.info "listening port : #{listening_port}"
-logger.a_log.info "scraper server port : #{scrape_server_port}"
 logger.a_log.info "periodicity : #{periodicity}"
 logger.a_log.info "debugging : #{$debugging}"
 logger.a_log.info "staging : #{$staging}"
@@ -70,7 +61,7 @@ EventMachine.run {
 
     end
     begin
-       # en developpement, afin de tester plus facilement, on n'attend pas le jour de décenchement des events
+      # en developpement, afin de tester plus facilement, on n'attend pas le jour de décenchement des events
       calendar.execute_all_events_which_all_pre_tasks_are_over($staging == 'development' ? nil : start_date)
 
     rescue Exception => e
